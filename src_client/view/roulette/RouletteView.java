beefd17487be47686e1545ee617e56e988da4919
polvalesrodon <polvalesrodon@192.168.168.9>
2016-05-19 18:21:47 +0200

    Primer comentari javadoc (authors, a rouletteview)

--- src_client/view/roulette/RouletteView.java
+++ src_client/view/roulette/RouletteView.java
@@ -19,6 +19,24 @@ import model.struct.roulette.Casella;
 import view.Dialeg;
 import view.GameView;
 
+/**
+ * 
+ * <p>
+ * <b> Classe: RouletteView </b> <br/>
+ * Implementa la gràfica de la ruleta.
+ * </p>
+ * 
+ * @version 1.0 19/05/2016
+ * @author  Pol Valés - ls30599@salleurl.edu <br/>
+ * 			Diego Bellino - ls30741@salleurl.edu <br/>
+ * 			Enric Marin - ls31308@salleurl.edu <br/>
+ * 			Jordi Rubió - ls31289@salleurl.edu <br/>
+ * 			David Estepa - ls30622@salleurl.edu <br/>
+ * 			Disseny i programació orientats a objectes. <br/>
+ * 			La Salle - Universitat Ramon Llull. <br/>
+ * 
+ */
+
 public class RouletteView extends GameView {
 	private static final long serialVersionUID = 1L;
 	
