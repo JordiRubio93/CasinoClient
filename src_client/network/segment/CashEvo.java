c24dd5f87647e665824779c05e4e7b0ce1edfd1e
David Estepa <David Estepa@MSI>
2016-08-26 20:51:31 +0200

    Javadoc client (2na versió)

--- src_client/network/segment/CashEvo.java
+++ src_client/network/segment/CashEvo.java
@@ -1,7 +1,13 @@
-
-
-
-
+/**
+ * @author
+ * Pol Vales - ls30599@salleurl.edu
+ * Enric Marin - ls31308@salleurl.edu
+ * Diego Bellino - ls30741@salleurl.edu
+ * Jordi Rubio - ls31289@salleurl.edu
+ * David Estepa - ls30622@salleurl.edu
+ * DPO2 (Disseny i programacio orientats a objectes)
+ * La Salle, Universitat Ramon Llull
+ */
 
 package network.segment;
 
